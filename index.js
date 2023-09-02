9edce76dfb7723da9a27a511c0bc46c649fcdb72
aggagah <gagahsp4@gmail.com>
2023-09-02 15:56:22 +0700

    feat(auth): create user model, controller and route for signin and signup

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors	";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+// import routes
+import authRoute from "./routes/auth.route.js";
+
+// define application
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// basic api endpoint
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "API MNote", status: "OK" });
+});
+
+// auth routes
+app.use("/auth", authRoute);
+
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT || 80, () => {
+      console.log(`Server running on localhost port ${process.env.PORT}`);
+    });
+  });
